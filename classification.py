620d096b643cc9ec105996e59f1a13d8df6ff806
Dragos Vancea <dragos.vancea.15@ucl.ac.uk>
2019-01-04 23:06:07 +0000

    Bug Improvements

--- classification.py
+++ classification.py
@@ -40,54 +40,29 @@ def get_labels(i,test_samples):
 
 def SVM_eyeglasses(x_tr, y_tr, x_te):
     
-    clf = svm.SVC(C=1, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr', degree=2, gamma='scale', kernel='poly', max_iter=-1, probability=False, random_state=None, shrinking=True, tol=0.001, verbose=False) 
+    clf = svm.SVC(C=1,  degree=2, gamma='scale', kernel='poly')
     clf.fit(x_tr, y_tr)
     return np.ravel(clf.predict(x_te))
 
 def SVM_emotion(x_tr, y_tr, x_te):
     
-    clf = svm.SVC(C=1, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr', degree=2, gamma='scale', kernel='poly', max_iter=-1, probability=False, random_state=None, shrinking=True, tol=0.001, verbose=False) 
+    clf = svm.SVC(C=1,  degree=2, gamma='scale', kernel='poly')
     clf.fit(x_tr, y_tr)
     return np.ravel(clf.predict(x_te))
 
 def SVM_age(x_tr, y_tr, x_te):
     
-    clf = svm.SVC(C=1, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr', degree=2, gamma='scale', kernel='poly', max_iter=-1, probability=False, random_state=None, shrinking=True, tol=0.001, verbose=False) 
+   clf = svm.SVC(C=1,  degree=2, gamma='scale', kernel='rbf')
     clf.fit(x_tr, y_tr)
     return np.ravel(clf.predict(x_te))
 
 def SVM_human(x_tr, y_tr, x_te):
     
-    clf = svm.SVC(C=1, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr', degree=2, gamma='scale', kernel='poly', max_iter=-1, probability=False, random_state=None, shrinking=True, tol=0.001, verbose=False) 
+    clf = svm.SVC(C=1,  degree=2, gamma='scale', kernel='poly')
     clf.fit(x_tr, y_tr)
     return np.ravel(clf.predict(x_te))
 
-
-
-
-
-
-def accuracy_calc_te(y_pred,y_te,size):
-
-    np.ravel(y_te)
-    num = 0
-    for i in range (0, size - 1):
-        if y_pred[i] == y_te[i]:
-            num = num + 1
-    return num/size
-
-def accuracy_calc_tr(y_pred,y_tr,size):
-
-    np.ravel(y_tr)
-    num = 0
-    for i in range (0, size - 1):
-        if y_pred[i] == y_tr[i]:
-            num = num + 1
-    return num/size
-
-
-
-
+        
 def classify_eyeglasses(test_samples):
 
     X_train, X_test=divide_x(test_samples)
@@ -104,9 +79,9 @@ def classify_eyeglasses(test_samples):
     print(accuracy_score(Y_test, Y_pred_te)*100)
     print("Accuracy on train data for glasses detection:")
     print(accuracy_score(Y_train, Y_pred_tr)*100)
-
     print("The confusion matrix is:")
     print(confusion_matrix(Y_test, Y_pred_te))
+    
 
 
 def classify_emotion(test_samples):
@@ -125,10 +100,11 @@ def classify_emotion(test_samples):
     print(accuracy_score(Y_test, Y_pred_te)*100)
     print("Accuracy on train data for emotion detection:")
     print(accuracy_score(Y_train, Y_pred_tr)*100)
-    
     print("The confusion matrix is:")
     print(confusion_matrix(Y_test, Y_pred_te))
+    
 
+   
 def classify_age(test_samples):
 
     X_train, X_test=divide_x(test_samples)
@@ -141,22 +117,21 @@ def classify_age(test_samples):
     size_te = len(Y_pred_te)
     Y_pred_tr = SVM_age(X_train,Y_train,X_train)
     size_tr = len(Y_pred_tr)
-    #accuracy_te = accuracy_calc_te(Y_pred_te, Y_test, size_te)*100
     print("Accuracy on test data for age detection:")
     print(accuracy_score(Y_test, Y_pred_te)*100)
-    #accuracy_tr = accuracy_calc_tr(Y_pred_tr, Y_train, size_tr)*100
     print("Accuracy on train data for age detection:")
     print(accuracy_score(Y_train, Y_pred_tr)*100)
-
     print("The confusion matrix is:")
     print(confusion_matrix(Y_test, Y_pred_te))
+    
+
+    
 
 def classify_human(test_samples):
     
     X_train, X_test=divide_x(test_samples)
     X_train=X_train.reshape(X_train.shape[0],X_train.shape[1]*X_train.shape[2])
     X_test=X_test.reshape(X_test.shape[0],X_test.shape[1]*X_test.shape[2])
-    Y_train, Y_test=get_labels(4,test_samples)
     Y_train, Y_test=get_labels(5,test_samples)
     Y_train=Y_train[:,0]
     Y_test=Y_test[:,0]
@@ -164,18 +139,16 @@ def classify_human(test_samples):
     size_te = len(Y_pred_te)
     Y_pred_tr = SVM_human(X_train,Y_train,X_train)
     size_tr = len(Y_pred_tr)
-    accuracy_te = accuracy_calc_te(Y_pred_te, Y_test, size_te)*100
     print("Accuracy on test data for human detection:")
     print(accuracy_score(Y_test, Y_pred_te)*100)
     print("Accuracy on train data for human detection:")
     print(accuracy_score(Y_train, Y_pred_tr)*100)
-
     print("The confusion matrix is:")
     print(confusion_matrix(Y_test, Y_pred_te))
-
+    
 
 classify_eyeglasses(3000)
-classify_emotion(3000)
-classify_age(3000)
-#classify_human(3000)
+classify_emotion(4000)
+classify_age(4000)
+classify_human(3000)
 
